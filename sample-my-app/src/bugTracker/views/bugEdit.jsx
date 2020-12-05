e73774e1188849364a46c151f56e7477799d946a
Vishnu Teja Kugarthi <kugateja@in.ibm.com>
2020-12-05 17:16:04 +0530

    Assignment

--- sample-my-app/src/bugTracker/views/bugEdit.jsx
+++ sample-my-app/src/bugTracker/views/bugEdit.jsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
-import {projects} from '../../projects/index'
-import mapStateToProps from "../../bugTracker";
-const BugEdit = ({ addNew , store}) => {
+import { useState } from 'react';
+const BugEdit = ({ addNew }) => {
   const [newBugName, setNewBugName] = useState("");
   return (
     <section className="edit">
       <label htmlFor="">Bug Name :</label>
-      <input type="text" onChange={(evt) => setNewBugName(evt.target.value)} />
-
-      <label htmlFor="">Projects :</label>
-      <select name="" id="">
-        <option value={mapStateToProps}></option>
+      <input type="text" onChange={evt => setNewBugName(evt.target.value)} />
+      <label htmlFor="">Project : </label>
+      <select>
+        <option></option>
+        <option></option>
       </select>
-
       <input type="button" value="Add New" onClick={() => addNew(newBugName)} />
     </section>
   );
